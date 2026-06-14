a6a86205d6b1330e2e37f38e040412f5e0b7e23a
Siyan <146075999+chensy618@users.noreply.github.com>
2026-06-14 13:28:41 +0200

    add mps gpu support, Fixes #94

--- src/training.py
+++ src/training.py
@@ -58,7 +58,14 @@ class Trainer(BaseTrainer):
         self.epochs = epochs
         self.lr = lr
         self.checkpoint_path = PROJECT_ROOT / checkpoint_path
-        self.device = torch.device(device or ("cuda" if torch.cuda.is_available() else "cpu"))
+        if not device:
+            if torch.cuda.is_available():
+                device = "cuda"
+            elif torch.backends.mps.is_available():
+                device = "mps"
+            else:
+                device = "cpu"
+        self.device = torch.device(device)
         # Allow customizing the loss function; default to CrossEntropyLoss
         self.loss_fn = loss_fn or getattr(torch.nn, "CrossEntropyLoss")()
 
